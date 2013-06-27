9484cd267784edb13d3722997bbf34d1aafd113c
Fabian Lopez <lombareload@hotmail.com>
2013-06-26 22:20:11 -0500

    subidas y enlazadas librerias para interaccion con google docs

---
+++ death-ray-portlet/docroot/WEB-INF/src/com/deathRay/utils/GoogleDocsUtil.java
@@ -0,0 +1,23 @@
+package com.deathRay.utils;
+
+import com.google.gdata.client.docs.DocsService;
+//import com.google.gdata.data.docs;
+//import com.google.gdata.util;
+
+public class GoogleDocsUtil {
+
+	// singleton
+	private GoogleDocsUtil(){}
+	
+	private static class GoogleDocsUtilHolder{
+		private static final GoogleDocsUtil INSTANCE = new GoogleDocsUtil();
+	}
+	
+	public static GoogleDocsUtil getInstance(){
+		return GoogleDocsUtilHolder.INSTANCE;
+	}
+	// fin singleton
+	
+	
+	
+}
