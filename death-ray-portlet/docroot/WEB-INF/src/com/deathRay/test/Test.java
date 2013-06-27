79dcd0bd2819470ed509e82e5f148a953b79ac71
Michael <mrondon@grupodot.com>
2013-06-27 01:22:39 -0500

    Crear o actualizar tarea.

--- death-ray-portlet/docroot/WEB-INF/src/com/deathRay/test/Test.java
+++ death-ray-portlet/docroot/WEB-INF/src/com/deathRay/test/Test.java
@@ -1,6 +1,10 @@
 package com.deathRay.test;
 
+import com.deathRay.cloud.Tarea;
+import com.deathRay.dao.TareaDao;
 import com.deathRay.util.Util;
+import com.google.api.services.datastore.DatastoreV1.Entity;
+import com.google.api.services.datastore.client.DatastoreException;
 
 import java.util.Date;
 
@@ -11,9 +15,27 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Util u = new Util();		
-		System.out.println(u.dateToTimestampMicroseconds(new Date()));
+		Tarea tarea = new Tarea();
+		TareaDao tareaDao;
+		try {
+			tareaDao=tarea.createOrUpdateTarea(new Date(), new Date(), 1L, 2L, 3L,4L);
+			System.out.println("getEstado : "+tareaDao.getEstado());
+			System.out.println("getGroupId : "+tareaDao.getGroupId());
+			System.out.println("getTaskId : "+tareaDao.getTaskId());
+			System.out.println("getUserId : "+tareaDao.getUserId());
+			System.out.println("getClass : "+tareaDao.getClass());
+			System.out.println("getFecha_final : "+tareaDao.getFecha_final());
+			System.out.println("getFecha_inicial : "+tareaDao.getFecha_inicial());
+		} catch (DatastoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
+		/*System.out.println(System.getenv("DATASTORE_PRIVATE_KEY_FILE"));
+		System.out.println(System.getenv("PATH"));*/
+		
+		
 	}
 
 }
